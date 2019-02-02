9c4d6b0b599eb4083f8f79017ec93304c75fec8a
Miguel Espinoza <miguespinoza1@gmail.com>
2019-02-02 13:08:12 -0600

    update info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Blog of random intresting stuff`,
+    author: `Miguel Espinoza`,
+    description: `This the place where I write everithing that I find intrsting`,
+    siteUrl: `https://miguespinoza.azurewebsites.net/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mikespinoza18`,
     },
   },
   plugins: [
