c7d5f88149e0e2d0fc863f4cff98b072f73b6120
Miguel Espinoza <miguespinoza1@gmail.com>
2019-03-03 14:12:53 -0600

    delete default pagers

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Miguel Espinoza's Blog`,
+        short_name: `Miguespinoza`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
