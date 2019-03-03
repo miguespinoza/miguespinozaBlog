3de7334266f838c2cb24d680bac493b62cb8de4b
Miguel Espinoza <miguespinoza1@gmail.com>
2019-03-03 14:28:11 -0600

    add icon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -66,6 +66,7 @@ class Layout extends React.Component {
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <div>Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="#!">CC 3.0 BY</a></div>
         </footer>
       </div>
     )
